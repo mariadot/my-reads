8d043dd877d71186c616cc849cb9e27735382402
Maria Dot <mariadot.github@gmail.com>
2018-07-26 20:15:34 -0500

    feat: Add react-router and defined initial structure

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route } from 'react-router-dom';
+import Home from './components/Home';
+import Search from './components/Search';
+import logo from './images/logo.svg';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Route exact path='/' component={ Home } />
+        <Route path='/search' component={ Search } />
       </div>
     );
   }

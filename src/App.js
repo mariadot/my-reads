c708b0c88f92fb63aa8a6e1c976a7f7cd12fa900
Maria Dot <mariadot.github@gmail.com>
2018-08-02 21:55:23 -0500

    feat: Make initial api request Added BooksAPI util

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
-import Home from './components/Home';
-import Search from './components/Search';
+import Home from './views/Home';
+import Search from './views/Search';
 import logo from './images/logo.svg';
 import './styles/App.css';
+import * as BooksAPI from './utils/BooksAPI'
 
 class App extends Component {
+  state = {
+    books: null
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll()
+    .then((books) => {
+      this.setState(()=>({
+        books
+      }))
+    })
+  }
+
   render() {
     return (
       <div className="App">

5a87f7a0d0645e6dd189a95a412b889c1a11975e
Maria Dot <mariadot.github@gmail.com>
2018-08-29 21:34:54 -0500

    feat: Added Search page markup

--- src/views/Search.js
+++ src/views/Search.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Search = () => (
-	<div>
-		<p>Search</p>
-		<Link to='./'>Home</Link>
+	<div className='search-books'>
+		<div className='search-books-bar'>
+			<Link className='close-search' to='./'>Home</Link>
+			<div className='search-books-input-wrapper'>
+				<input type='text' placeholder='Search by title or author' />
+			</div>
+		</div>
+		<div className="search-books-results">
+			<ol className="books-grid"></ol>
+		</div>
+		
 	</div>
 );
 
